1ff6cdf7abaf791921229b1207702ffb0a45f87d
enzolda <enzolealdearagao@gmail.com>
2024-05-06 21:27:56 -0300

    Adicionei o cadastro e visualização de informações de contas de clientes

--- ClienteMenu.java
+++ ClienteMenu.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class ClienteMenu {
     private String title;
     private List<String> options;
+    public List<Cliente> clientes ;
 
     public ClienteMenu(List<String> options) {
         this.title = "Menu";
@@ -30,6 +31,35 @@ public class ClienteMenu {
             } catch (NumberFormatException e) {
                 op = 0;
             }
+
+            if (op == 1){ // Cadastro
+                System.out.println("Digite o nome do novo cliente:");
+                String novoNome = scan.nextLine();
+
+                System.out.println("Digite o CPF do novo cliente:");
+                String novoCpf = scan.nextLine();
+
+                System.out.println("Digite a idade do novo cliente:");
+                int novoIdade = scan.nextInt();
+                
+                try {
+                    Cliente novoCliente = new Cliente(novoNome, novoCpf, novoIdade);
+                    clientes.add(novoCliente);    
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            } 
+            else if (op == 2){ //Visualizar informações
+                System.out.println("Digite o CPF a ser pesquisado:");
+                String pesquisarCpf = scan.nextLine();
+                for (Cliente cliente: clientes){
+                    if (cliente.getCpf().equals(pesquisarCpf)) {
+                        cliente.imprimirDetalhesCliente();
+                        break;
+                    }
+                }
+            }
+            
             if (op >= i) {
                 System.out.println("Opcao errada!");
                 op = 0;
