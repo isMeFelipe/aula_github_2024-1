14fe3b9b6fd984363030dc0b7e9efcd622f46551
jordanabfranca <jordanafranca@ufpi.edu.br>
2024-05-06 21:32:13 -0300

    menu de opeções

---
+++ operacoesMenu.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class operacoesMenu {
+    private String title;
+    private List<String> options;
+
+    public operacoesMenu(List<String> options) {
+        this.title = "Menu";
+        this.options = options;
+    }
+
+    public operacoesMenu(String title, List<String> options) {
+        this.title = title;
+        this.options = options;
+    }
+
+    public int getSelection(Scanner scan) {
+        int op = 0;
+        while (op == 0) {
+            System.out.println(title + "\n");
+            int i = 1;
+            for (String option : options) {
+                System.out.println(i++ + " - " + option);
+            }
+
+            System.out.println("\nInforme a opcao desejada. ");
+            try {
+                op = Integer.parseInt(scan.nextLine());
+            } catch (NumberFormatException e) {
+                op = 0;
+            }
+            if (op >= i) {
+                System.out.println("Opcao errada!");
+                op = 0;
+            }
+
+        }
+        return op;
+    }
+
+}
